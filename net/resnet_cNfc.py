1fa7f9c6c77774ddc525da0e9670aa3a0faf009e
TigerStone93 <46550846+TigerStone93@users.noreply.github.com>
2023-07-25 19:46:08 +0900

    Update resnet_cNfc.py

--- net/resnet_cNfc.py
+++ net/resnet_cNfc.py
@@ -25,16 +25,16 @@ import torch.nn.utils as utils
 
 class SpectralNormalizedConvolutionalBlock(nn.Module):
     expansion = 1
-    def __init__(self, in_channels, out_channels):
+    def __init__(self, in_channels, out_channels, stride=1):
         super(SpectralNormalizedConvolutionalBlock, self).__init__()
-        self.convolutional_1 = utils.spectral_norm(nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False))
+        self.convolutional_1 = utils.spectral_norm(nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False))
         self.batch_normalization_1 = nn.BatchNorm2d(out_channels)
-        self.convolutional_2 = utils.spectral_norm(nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False))
+        self.convolutional_2 = utils.spectral_norm(nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False))
         self.batch_normalization_2 = nn.BatchNorm2d(out_channels)
 
         self.shortcut = nn.Sequential()
         if stride != 1 or in_channels != out_channels:
-            self.shortcut = nn.Sequential(utils.spectral_norm(nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=1, bias=False)),
+            self.shortcut = nn.Sequential(utils.spectral_norm(nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride, bias=False)),
                                           nn.BatchNorm2d(out_channels))
 
         self.leaky_relu = F.leaky_relu
@@ -83,29 +83,32 @@ class ResNet(nn.Module):
         num_outputs = 10,):
         super(ResNet, self).__init__()
 
-        self.convolutional_block_1 = SpectralNormalizedConvolutionalBlock(16, 16)
-        self.convolutional_block_2 = SpectralNormalizedConvolutionalBlock(16, 32)
-        self.convolutional_block_3 = SpectralNormalizedConvolutionalBlock(32, 64)
-        self.convolutional_block_4 = SpectralNormalizedConvolutionalBlock(64, 128)
+        self.convolutional_block_1 = SpectralNormalizedConvolutionalBlock(3, 16) # [16, 300, 300]
+        self.convolutional_block_2 = SpectralNormalizedConvolutionalBlock(16, 32) # [32, 150, 150]
+        self.convolutional_block_3 = SpectralNormalizedConvolutionalBlock(32, 64) # [64, 75, 75]
+        self.convolutional_block_4 = SpectralNormalizedConvolutionalBlock(64, 128) # [128, 38, 38]
         
-        self.fully_connected_block_1 = SpectralNormalizedFullyConnectedBlock(784, 784)
-        self.fully_connected_block_2 = SpectralNormalizedFullyConnectedBlock(392, 392)
-        self.fully_connected_block_3 = SpectralNormalizedFullyConnectedBlock(196, 196)
-        self.fully_connected_block_4 = SpectralNormalizedFullyConnectedBlock(98, 98)
-        self.output_layer = nn.Linear(98, num_outputs)
-        
-        self.leaky_relu = F.leaky_relu
+        self.fully_connected_block_1 = SpectralNormalizedFullyConnectedBlock(128*38*38 + 5, 512)
+        self.fully_connected_block_2 = SpectralNormalizedFullyConnectedBlock(512, 256)
+        self.fully_connected_block_3 = SpectralNormalizedFullyConnectedBlock(256, 128)
+        self.fully_connected_block_4 = SpectralNormalizedFullyConnectedBlock(128, num_outputs)
         
     # ============================================================ #
     
-    def forward(self, x):
-        out = self.convolutional_block_1(x)
+    def forward(self, map_input_tensor, record_input_tensor):
+        out = self.convolutional_block_1(map_input_tensor)
         out = self.convolutional_block_2(out)
         out = self.convolutional_block_3(out)
         out = self.convolutional_block_4(out)
-        out = out.view(out.size(0), -1) # Flattening
         
-        out = self.leaky_relu(self.fully_connected_block_1(out))
+        out = out.view(out.size(0), -1) # Flattening
+        # record_input_tensor = record_input_tensor.view(record_input_tensor.size(0), -1)
+        out = torch.cat((out, record_input_tensor), dim=1) # Concatenating
+        
+        out = self.fully_connected_block_1(out)
+        out = self.fully_connected_block_2(out)
+        out = self.fully_connected_block_3(out)
+        out = self.fully_connected_block_4(out)
         return out
 
 # ========================================================================================== #
