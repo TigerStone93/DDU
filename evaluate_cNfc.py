e40ae604415c1ec5d808d0a246c91d9eae01cd50
TigerStone93 <46550846+TigerStone93@users.noreply.github.com>
2023-08-03 18:05:14 +0900

    Update evaluate_cNfc.py

--- evaluate_cNfc.py
+++ evaluate_cNfc.py
@@ -148,6 +148,135 @@ def expected_calibration_error(confs, preds, labels, num_bins=10):
 
 # ========================================================================================== #
 
+# For GMM
+def get_embeddings(net, loader: torch.utils.data.DataLoader, num_dim: int, dtype, device, storage_device,):
+    num_samples = len(loader.dataset)
+    embeddings = torch.empty((num_samples, num_dim), dtype=dtype, device=storage_device)
+    labels = torch.empty(num_samples, dtype=torch.int, device=storage_device)
+
+    with torch.no_grad():
+        start = 0
+        for data, label in tqdm(loader):
+            data = data.to(device)
+            label = label.to(device)
+
+            if isinstance(net, nn.DataParallel):
+                out = net.module(data)
+                out = net.module.feature
+            else:
+                out = net(data)
+                out = net.feature
+
+            end = start + len(data)
+            embeddings[start:end].copy_(out, non_blocking=True)
+            labels[start:end].copy_(label, non_blocking=True)
+            start = end
+
+    return embeddings, labels
+
+# ========================================================================================== #
+
+JITTERS = [0, torch.finfo(torch.double).tiny] + [10 ** exp for exp in range(-308, 0, 1)]
+def _centered_cov_torch(x):
+    n = x.shape[0]
+    res = 1 / (n - 1) * x.t().mm(x)
+    return res
+
+def gmm_fit(embeddings, labels, num_classes):
+    with torch.no_grad():
+        classwise_mean_features = torch.stack([torch.mean(embeddings[labels == c], dim=0) for c in range(num_classes)])
+        classwise_cov_features = torch.stack([_centered_cov_torch(embeddings[labels == c] - classwise_mean_features[c]) for c in range(num_classes)])
+
+    with torch.no_grad():
+        for jitter_eps in JITTERS:
+            try:
+                jitter = jitter_eps * torch.eye(classwise_cov_features.shape[1], device=classwise_cov_features.device,).unsqueeze(0)
+                gmm = torch.distributions.MultivariateNormal(loc=classwise_mean_features, covariance_matrix=(classwise_cov_features + jitter),)
+            except RuntimeError as e:
+                if "cholesky" in str(e):
+                    continue
+            except ValueError as e:
+                if "The parameter covariance_matrix has invalid values" in str(e):
+                    continue
+            break
+
+    return gmm, jitter_eps
+
+# ========================================================================================== #
+
+def _gmm_forward(net, gaussians_model, data_B_X):
+    if isinstance(net, nn.DataParallel):
+        features_B_Z = net.module(data_B_X)
+        features_B_Z = net.module.feature
+    else:
+        features_B_Z = net(data_B_X)
+        features_B_Z = net.feature
+
+    log_probs_B_Y = gaussians_model.log_prob(features_B_Z[:, None, :])
+
+    return log_probs_B_Y
+
+def gmm_evaluate(net, gaussians_model, loader, device, num_classes, storage_device):
+    num_samples = len(loader.dataset)
+    logits_N_C = torch.empty((num_samples, num_classes), dtype=torch.float, device=storage_device)
+    labels_N = torch.empty(num_samples, dtype=torch.int, device=storage_device)
+
+    with torch.no_grad():
+        start = 0
+        for data, label in tqdm(loader):
+            data = data.to(device)
+            label = label.to(device)
+
+            logit_B_C = _gmm_forward(net, gaussians_model, data)
+
+            end = start + len(data)
+            logits_N_C[start:end].copy_(logit_B_C, non_blocking=True)
+            labels_N[start:end].copy_(label, non_blocking=True)
+            start = end
+
+    return logits_N_C, labels_N
+
+# ========================================================================================== #
+
+# For softmax
+def get_roc_auc(net, test_loader, ood_test_loader, uncertainty, device, confidence=False):
+    logits, _ = get_logits_labels(net, test_loader, device)
+    ood_logits, _ = get_logits_labels(net, ood_test_loader, device)
+
+    return get_roc_auc_logits(logits, ood_logits, uncertainty, device, confidence=confidence)
+
+# ========================================================================================== #
+
+# For GMM and softmax
+def get_roc_auc_logits(logits, ood_logits, uncertainty, device, confidence=False):
+    # For entropy and logsumexp
+    uncertainties = uncertainty(logits)
+    ood_uncertainties = uncertainty(ood_logits)
+    
+    # For in-distribution
+    bin_labels = torch.zeros(uncertainties.shape[0]).to(device) # binary labels
+    in_scores = uncertainties
+    
+    # For out-of-distribution
+    bin_labels = torch.cat((bin_labels, torch.ones(ood_uncertainties.shape[0]).to(device)))
+    ood_scores = ood_uncertainties
+    
+    # Preprocessing for logsumexp, not entropy
+    if confidence:
+        bin_labels = 1 - bin_labels
+    
+    scores = torch.cat((in_scores, ood_scores))
+    
+    # Calculating the fpr(false positive rate), tpr(true positive rate), and thresholds
+    fpr, tpr, thresholds = metrics.roc_curve(bin_labels.cpu().numpy(), scores.cpu().numpy())
+    precision, recall, prc_thresholds = metrics.precision_recall_curve(bin_labels.cpu().numpy(), scores.cpu().numpy())
+    auroc = metrics.roc_auc_score(bin_labels.cpu().numpy(), scores.cpu().numpy())
+    auprc = metrics.average_precision_score(bin_labels.cpu().numpy(), scores.cpu().numpy())
+
+    return (fpr, tpr, thresholds), (precision, recall, prc_thresholds), auroc, auprc
+
+# ========================================================================================== #
+
 models = {
     "resnet18": resnet18,}
 
@@ -431,13 +560,12 @@ if __name__ == "__main__":
         if (args.model_type == "gmm"): # gmm
             # Evaluating the GMM model
             print("GMM Model")
-            embeddings, labels = get_embeddings(
-                net,
-                train_loader,
-                num_dim=model_to_num_dim[args.model],
-                dtype=torch.double,
-                device=device,
-                storage_device=device,)
+            embeddings, labels = get_embeddings(net,
+                                                train_loader,
+                                                num_dim=model_to_num_dim[args.model],
+                                                dtype=torch.double,
+                                                device=device,
+                                                storage_device=device,)
 
             try:
                 gaussians_model, jitter_eps = gmm_fit(embeddings=embeddings, labels=labels, num_classes=num_classes)
